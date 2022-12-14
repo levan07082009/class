d7d1ddcbdc458c5e05c1fb940beccfca3fba3f52
levan070809 <100714017+levan07082009@users.noreply.github.com>
2022-12-14 08:24:41 -0800

    Update 9.py

--- 9.py
+++ 9.py
@@ -1,8 +1,8 @@
 name = input("Enter employee's name: ")
-hours = input("Enter number of hours worked in a week (e.g., 10): ")
-pay_rate = input("Enter hourly pay rate (e.g., 9.75):")
-tax = input("Enter federal tax withholding rate (e.g., 20%): ")
-rate = input("Enter state tax withholding rate (e.g., 9%): ")
+hours = eval(input("Enter number of hours worked in a week (e.g., 10): "))
+pay_rate = eval(input("Enter hourly pay rate (e.g., 9.75):"))
+tax = eval(input("Enter federal tax withholding rate (e.g., 20%): "))
+rate = eval(input("Enter state tax withholding rate (e.g., 9%): "))
 
 
 
@@ -11,5 +11,8 @@ rate = input("Enter state tax withholding rate (e.g., 9%): ")
 print(f"Employee Name: {name}")
 print(f"Hours Worked: {hours}")
 print(f"Pay Rate: {pay_rate}")
-print(f"Gross Pay:{int(pay_rate) * int(hours)}")
-
+print(f"Gross Pay:{pay_rate * hours}")
+print("Deductions:")
+print(f"   Federal Withholding ({tax*100}%): {hours * tax*pay_rate:.3}")
+print(f"   State Withholding ({tax*100}%): {hours * rate*pay_rate:.3}")
+print(f"   Total Deduction: {hours * tax*pay_rate + rate* hours *pay_rate:.4} Net Pay: {hours*pay_rate - (hours * tax*pay_rate + rate* hours *pay_rate):.4}")
