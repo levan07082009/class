c12a36519bf53641dbf9aafe42b20eab7776f68c
levan070809 <100714017+levan07082009@users.noreply.github.com>
2022-12-14 08:50:41 -0800

    Update 5.py

--- 5.py
+++ 5.py
@@ -2,4 +2,4 @@ sides=eval(input("enter the number of sides"))
 a=eval(input("Enter the side: "))
 import math as m
 area=(sides * a ** 2)/ (4 * m.tan(m.pi/sides))
-print("The area of the polygon is " + str(area))
+print(f"The area of the polygon is {area}")
